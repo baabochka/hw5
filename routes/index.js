3aeac1c28e3a19200ecda32c39a59ed491b4af33
Valentina Pogudina <valentina.pogudina@stonybrook.edu>
2019-04-02 15:49:54 -0400

    meh

--- routes/index.js
+++ routes/index.js
@@ -73,8 +73,7 @@ router.get('/retrieve', function(req, res){ ///:filename
       res.json({status: 'ERROR'});
     }
 
-    res.set('Content-Type',req.query.filename.split('.')[1]);
-    res.send(result.first().contents);
+    res.type(req.query.filename.split('.')[1]).send(result.first().contents);
     // res.send({status: "OK", contents: img});
     //The row is an Object with column names as property keys.
     // console.log('My file is this: ', img);
